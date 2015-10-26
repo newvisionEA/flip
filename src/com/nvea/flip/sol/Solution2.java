c7774e83f8ca46987f30624ecd5ac5f9b35eafdd
raz <raz@lepty.ro>
2015-10-26 17:04:25 +0200

    Change-Id: I1576cf52481c774b10f68e797b28d4c6e976d0f4

--- src/com/nvea/flip/sol/Solution2.java
+++ src/com/nvea/flip/sol/Solution2.java
@@ -18,79 +18,79 @@ import com.nvea.flip.model.Warehouse;
 
 public class Solution2 {
 
-  ArrayList<Warehouse> path = new ArrayList<Warehouse>();
-  private Article[] items;
-  private Warehouse w;
-  private ArrayList<Operation> ops;
-  private int minSol = 10000;
+	ArrayList<Warehouse> path = new ArrayList<Warehouse>();
+	private Article[] items;
+	private Warehouse w;
+	private ArrayList<Operation> ops;
+	private int minSol = 10000;
 
-  public Solution2(Warehouse w, Article[] items) {
-    this.w = w;
-    this.items = items;
-    this.ops = new ArrayList<Operation>();
-  }
+	public Solution2(Warehouse w, Article[] items) {
+		this.w = w;
+		this.items = items;
+		this.ops = new ArrayList<Operation>();
+	}
 
-  public int find() {
-    path.add((Warehouse) w.clone());
-    find(0);
-    return minSol;
-  }
+	public int find() {
+		path.add((Warehouse) w.clone());
+		find(0);
+		return minSol;
+	}
 
-  private void find(int itemCounter) {
-    if (itemCounter == items.length) {
-      System.out.println(ops);
-      if (minSol > ops.size() - items.length) {
-        minSol = ops.size() - items.length;
-      }
-      System.out.println(minSol);
-      return;
-    }
+	private void find(int itemCounter) {
+		if (itemCounter == items.length) {
+			System.out.println(ops);
+			if (minSol > ops.size() - items.length) {
+				minSol = ops.size() - items.length;
+			}
+			System.out.println(minSol);
+			return;
+		}
 
-    ArrayList<Operation> ops2 = getPossibleOperations(itemCounter);
+		ArrayList<Operation> ops2 = getPossibleOperations(itemCounter);
 
-    for (Operation op : ops2) {
-      try {
-        op.action(w);
-        if (!ops.contains(op) && ops.size() < 9) {
-          ops.add(op);
-          if (!path.contains(w)) {
-            path.add((Warehouse) w.clone());
-            find(op.increment(itemCounter));
-            path.remove(path.size() - 1);
-          }
-          ops.remove(op);
-        }
-        op.undoAction(w);
-      } catch (Exception e) {
-        op.onCatch(w);
-      }
-    }
-  }
+		for (Operation op : ops2) {
+			try {
+				op.action(w);
+				if (!ops.contains(op) && ops.size() < minSol) {
+					ops.add(op);
+					if (!path.contains(w)) {
+						path.add((Warehouse) w.clone());
+						find(op.increment(itemCounter));
+						path.remove(path.size() - 1);
+					}
+					ops.remove(op);
+				}
+				op.undoAction(w);
+			} catch (Exception e) {
+				op.onCatch(w);
+			}
+		}
+	}
 
-  private ArrayList<Operation> getPossibleOperations(int itemCounter) {
-    ArrayList<Operation> result = new ArrayList<Operation>();
-    for (int y = 0; y < w.getHeight(); y++) {
-      for (int x = 0; x < w.getWidth(); x++) {
-        if (w.getArticleAt(x, y) == null) {
-          result.add(new Store(items[itemCounter], x, y));
-        }
-      }
-    }
+	private ArrayList<Operation> getPossibleOperations(int itemCounter) {
+		ArrayList<Operation> result = new ArrayList<Operation>();
+		for (int y = 0; y < w.getHeight(); y++) {
+			for (int x = 0; x < w.getWidth(); x++) {
+				if (w.getArticleAt(x, y) == null) {
+					result.add(new Store(items[itemCounter], x, y));
+				}
+			}
+		}
 
-    for (int y = 0; y < w.getHeight(); y++) {
-      for (int x = 0; x < w.getWidth(); x++) {
-        if (w.getArticleAt(x, y) != null) {
-          Article artToMove = w.getArticleAt(x, y);
-          for (int yy = 0; yy < w.getHeight(); yy++) {
-            for (int xx = 0; xx < w.getWidth(); xx++) {
-              if (w.getArticleAt(xx, yy) == null) {
-                result.add(new Move(artToMove, x, y, xx, yy));
-              }
-            }
-          }
-        }
-      }
-    }
-    return result;
-  }
+		for (int y = 0; y < w.getHeight(); y++) {
+			for (int x = 0; x < w.getWidth(); x++) {
+				if (w.getArticleAt(x, y) != null) {
+					Article artToMove = w.getArticleAt(x, y);
+					for (int yy = 0; yy < w.getHeight(); yy++) {
+						for (int xx = 0; xx < w.getWidth(); xx++) {
+							if (w.getArticleAt(xx, yy) == null) {
+								result.add(new Move(artToMove, x, y, xx, yy));
+							}
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
 }
--- test/com/nvea/flip/SolutionTest.java
+++ test/com/nvea/flip/SolutionTest.java
@@ -6,7 +6,7 @@
 
 package com.nvea.flip;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -21,91 +21,95 @@ import com.nvea.flip.sol.Solution2;
  */
 
 public class SolutionTest {
-  @Test
-  public void testSolution() {
-    Warehouse w = new Warehouse(4, 4);
+	@Test
+	public void testSolution() {
+		Warehouse w = new Warehouse(4, 4);
 
-    w.store(0, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
-    w.store(1, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(2, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
-    w.store(3, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(0, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(1, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(3, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
 
-    Article[] items = new Article[] { new Article(new ArticleType[] { ArticleType.FREEZING }) };
-    Solution2 sol = new Solution2(w, items);
-    assertEquals(1, sol.find());
-  }
+		Article[] items = new Article[] { new Article(
+		    new ArticleType[] { ArticleType.FREEZING }) };
+		Solution2 sol = new Solution2(w, items);
+		assertEquals(1, sol.find());
+	}
 
-  @Test
-  public void testSolution2() {
-    Warehouse w = new Warehouse(4, 4);
+	@Test
+	public void testSolution2() {
+		Warehouse w = new Warehouse(4, 4);
 
-    w.store(0, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
-    w.store(1, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(2, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
-    w.store(3, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(0, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(1, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(3, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
 
-    Article[] items = new Article[] { new Article(new ArticleType[] { ArticleType.FREEZING }), //
-      new Article(new ArticleType[] { ArticleType.FREEZING }) };
+		Article[] items = new Article[] {
+		    new Article(new ArticleType[] { ArticleType.FREEZING }), //
+		    new Article(new ArticleType[] { ArticleType.FREEZING }) };
 
-    Solution2 sol = new Solution2(w, items);
-    assertEquals(2, sol.find());
-  }
+		Solution2 sol = new Solution2(w, items);
+		assertEquals(2, sol.find());
+	}
 
-  @Test
-  public void testSolution3() {
-    Warehouse w = new Warehouse(4, 4);
+	@Test
+	public void testSolution3() {
+		Warehouse w = new Warehouse(4, 4);
 
-    w.store(0, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
-    w.store(1, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
-    w.store(2, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
-    w.store(3, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(0, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
+		w.store(1, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
+		w.store(2, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
+		w.store(3, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
 
-    w.store(0, 2, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(1, 2, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(2, 2, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(0, 2, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(1, 2, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 2, new Article(new ArticleType[] { ArticleType.LIQUID }));
 
-    w.store(0, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(1, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(2, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(3, 1, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(0, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(1, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(3, 1, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
 
-    w.store(0, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(1, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(2, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(3, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(0, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(1, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(3, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
 
-    Article[] items = new Article[] { new Article(new ArticleType[] { ArticleType.FREEZING }), //
-    };
+		Article[] items = new Article[] { new Article(
+		    new ArticleType[] { ArticleType.FREEZING }), //
+		};
 
-    Solution2 sol = new Solution2(w, items);
-    assertEquals(2, sol.find());
-  }
+		Solution2 sol = new Solution2(w, items);
+		assertEquals(2, sol.find());
+	}
 
-  @Test
-  public void testSolution4() {
-    Warehouse w = new Warehouse(4, 4);
+	@Test
+	public void testSolution4() {
+		Warehouse w = new Warehouse(4, 4);
 
-    w.store(0, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
-    w.store(1, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
-    w.store(2, 3, new Article(new ArticleType[] { ArticleType.FREEZING }));
-    w.store(3, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(0, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(1, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 3, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(3, 3, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
 
-    w.store(0, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(1, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(2, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(3, 1, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
+		w.store(0, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(1, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 1, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(3, 1, new Article(new ArticleType[] { ArticleType.INFLAMABLE }));
 
-    w.store(0, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(1, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(2, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
-    w.store(3, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(0, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(1, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(2, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
+		w.store(3, 0, new Article(new ArticleType[] { ArticleType.LIQUID }));
 
-    Article[] items = new Article[] { new Article(new ArticleType[] { ArticleType.FREEZING }), //
-      new Article(new ArticleType[] { ArticleType.FREEZING }), //
-      new Article(new ArticleType[] { ArticleType.FREEZING }), //
-      new Article(new ArticleType[] { ArticleType.FREEZING }) };
+		Article[] items = new Article[] {
+		    new Article(new ArticleType[] { ArticleType.FREEZING }), //
+		    new Article(new ArticleType[] { ArticleType.FREEZING }), //
+		    new Article(new ArticleType[] { ArticleType.FREEZING }), //
+		    new Article(new ArticleType[] { ArticleType.FREEZING }) };
 
-    Solution2 sol = new Solution2(w, items);
-    assertEquals(2, sol.find());
-  }
+		Solution2 sol = new Solution2(w, items);
+		assertEquals(2, sol.find());
+	}
 }
